88e9669755cb02360f83da44fe81a38077699bbe
Igor-Ulyanov <igorulyanov5@gmail.com>
2026-05-09 12:42:23 +0200

    Swagger anotations

--- project-files/backend/auth-api/src/main/java/com/igorul/authapi/controller/OrganizationController.java
+++ project-files/backend/auth-api/src/main/java/com/igorul/authapi/controller/OrganizationController.java
@@ -2,33 +2,40 @@ package com.igorul.authapi.controller;
 
 import com.igorul.authapi.model.Organization;
 import com.igorul.authapi.service.OrganizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/orgs")
+@Tag(name = "Organizations", description = "Org management endpoints for CRUD operations")
 public class OrganizationController {
 
     private final OrganizationService organizationService;
 
     public OrganizationController(OrganizationService organizationService) {this.organizationService = organizationService;}
 
+    @Operation(summary = "Create a new org")
     @PostMapping
     public Organization createOrganization(@RequestBody Organization organization) {
         return organizationService.createOrganization(organization);
     }
 
+    @Operation(summary = "View list of all orgs")
     @GetMapping
     public List<Organization> getAllOrganizations() {
         return organizationService.getAllOrganizations();
     }
 
+    @Operation(summary = "Delete an org by their id")
     @DeleteMapping("/{id}")
     public void deleteOrganization(@PathVariable Long id) {
         organizationService.deleteOrganization(id);
     }
 
+    @Operation(summary = "Edit an org's data by their id (partial editing accepted)")
     @PutMapping("/{id}")
     public Organization updateUser(@PathVariable Long id,
                            @RequestBody Organization org) {
--- project-files/backend/auth-api/src/main/java/com/igorul/authapi/controller/UserController.java
+++ project-files/backend/auth-api/src/main/java/com/igorul/authapi/controller/UserController.java
@@ -4,10 +4,14 @@ import com.igorul.authapi.model.User;
 import com.igorul.authapi.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users", description = "User management endpoints for CRUD operations")
 public class UserController {
 
     private final UserService userService;
@@ -16,21 +20,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create a new user")
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
+    @Operation(summary = "View list of all users")
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Delete a user by their id")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
 
+    @Operation(summary = "Edit a user's data by their id (partial editing accepted)")
     @PutMapping("/{id}")
     public User updateUser(@PathVariable Long id,
                            @RequestBody User user) {
